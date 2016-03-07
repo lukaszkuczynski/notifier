0cad6c3b46a817e02bbf4b886b947ffb2a6dbc85
pishon <szybkistart24@gmail.com>
2016-03-07 22:02:03 +0100

    parses web article

---
+++ application/input/webarticlesreceiver.py
@@ -0,0 +1,73 @@
+'''
+Created on 21 lut 2016
+
+@author: luk
+'''
+
+from lxml import html
+import requests
+from application.model.article import Article
+
+
+class WebArticlesReceiver:
+    '''
+    Receives web articles from page
+    '''
+
+
+    def __init__(self, params=None):
+        self.url = params['input']['url']
+        self.articles_path = params['input']['article']['path']
+        self.titles = params['input']['articleElement']['titles']
+        self.contents = params['input']['articleElement']['contents']
+
+    def parse_from_element_using_css(self, element, cssselects):
+        for select in cssselects :
+            title_candidate = element.cssselect(select)
+            if len(title_candidate) == 1 :
+                if title_candidate[0].text:
+                    return title_candidate[0].text
+        return ''
+
+    def receive(self):
+        articles = []
+        page = requests.get(self.url)
+        page_tree = html.fromstring(page.content)
+        articles_nodes = page_tree.cssselect(self.articles_path)
+        for article_node in articles_nodes:
+            title = self.parse_from_element_using_css(article_node, self.titles)
+            content = self.parse_from_element_using_css(article_node, self.contents)
+            a = Article(title=title, text=content)
+            print(a)
+            articles.append(a)
+
+
+
+        # return articles
+        return articles
+
+
+if __name__ == '__main__':
+    
+    url = 'http://wiadomosci.wp.pl/?ticaid=1169c4&_ticrsn=3'
+    article = 'ul.wiadomosciLst li'
+    titles = ['h2 a', 'a']
+    contents = titles
+    link = ''
+    params = { 
+        'input': {
+            'type': 'web.element',
+            'url': url,
+            'article': {
+                'path':article,
+            },
+            'articleElement': {
+                'titles':titles,
+                'contents':contents,
+                'link':link,
+            }
+        }
+    };
+
+    w = WebArticlesReceiver(params)
+    print(w.receive())
---
+++ application/model/article.py
@@ -0,0 +1,18 @@
+class Article:
+
+    def __init__(self, title, link='', text=''):
+        self.title = title
+        self.link = link
+        self.text = text
+
+    def __str__(self):
+        return self.__dict__.__str__()
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def __hash__(self):
+        return hash(self.title) ^ hash(self.link) ^ hash(self.text)
